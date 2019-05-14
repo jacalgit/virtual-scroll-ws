bddbe47f64a7d95bfcb9e3bec6d473ea22c8c0f9
Unknown <alexandre.jacquot@gmail.com>
2019-05-14 16:15:23 +0200

    Spring intializr

---
+++ src/main/java/com/github/jacal/demo/virtualscroll/VirtualScrollApplication.java
@@ -0,0 +1,13 @@
+package com.github.jacal.demo.virtualscroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualScrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualScrollApplication.class, args);
+	}
+
+}
